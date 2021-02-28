9bdeb7d6b67e4d70cc7f89029c2a6968ca823476
ARM <alexander.michaud@aya.yale.edu>
2021-02-28 08:06:50 -0500

    Successfully connected to AWS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+		s3 = Aws::S3::Resource.new
+		bucket = s3.bucket('kinorails-static-assets')
+		bucket.objects.limit(50).each do |obj|
+			puts "  #{obj.key} => #{obj.etag}"
+		end
   end
 end
