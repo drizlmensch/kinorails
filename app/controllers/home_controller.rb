7c7e3839c52fbc7fd519b6db1321d87a46ee99ca
ARM <alexander.michaud@aya.yale.edu>
2021-02-28 09:01:02 -0500

    Load all images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,17 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+		@img_urls = []
+		resource = Aws::S3::Resource.new
+		bucket = resource.bucket('kinorails-static-assets')
 		signer = Aws::S3::Presigner.new
-		@url, @headers = signer.presigned_request(
-			:get_object, bucket: "kinorails-static-assets", key: "cinema_paradiso.jpg"
-		)
+		
+		bucket.objects.each do |obj|
+			url, headers = signer.presigned_request(
+				:get_object, bucket: "kinorails-static-assets", key: obj.key
+			)
+			@img_urls.push(url)
+		end
   end
 
 end
